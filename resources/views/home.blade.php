87160695ffb9b70bda5d72ec4790d49a171d6b3f
Mengheang <mengheangweb@gmail.com>
2020-12-20 17:03:14 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+
+@extends('layout.app')
+
+@section('content')
+    <div class="jumbotron">
+        <h1 class="display-4">Hello, Mengheang!</h1>
+      </div>
+
+@endsection
+
+
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Budget</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ url('/')  }}">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/transaction">Transaction</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        @yield('content')
+    </div>
+</body>
+</html>
